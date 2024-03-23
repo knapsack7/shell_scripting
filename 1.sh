6b35ab024f4ee06e73a9ed1fc045b52b0405abeb
knapsack7 <manoj@Manojs-MacBook-Air.local>
2024-03-23 12:26:43 +0530

    install git on host machine

---
+++ 1.sh
@@ -0,0 +1,8 @@
+echo What is your name ?
+read NAME
+echo Hello $NAME I am printing the present working directory and its contents in new line
+pwd
+ls
+echo Hi $NAME I forgot to mention one thing this is hell lot of job for me let me sleep for 5 sec and then I will today date and time
+sleep 5
+date 
---
+++ prac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Hello Enter your age"
+read age
+echo "Hey you are $age years old."
